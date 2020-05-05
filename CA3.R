375abbce98f6aa8fb6acc7bcfe5b7a085f120761
eeno <iancorcoran87@gmail.com>
2020-05-05 22:53:07 +0100

    update

--- CA3.R
+++ CA3.R
@@ -1,3 +1,4 @@
+#loading data --------------------------------------------------------------------------------------------
 #data use
 #consumer price index
 
@@ -51,14 +52,22 @@ poi2$Year <- substring(poi2$Year, 2)
 
 planning_permission <- read.csv("Planning_Application_Sites_2010_onwards.csv")
 
+#check structure of planning permision
+str(planning_permission)
+
+# Cleaning Data------------------------------------------------------------------------------------------
+
 #replace missing values with NA's
 planning_permission[planning_permission == ""] <- NA
 
 #check unique values in planning permission
 unique(planning_permission_granted$Decision)
 
+
+#check the plannig authorities
 authority_freq <- table(planning_permission$PlanningAuthority)
 
+#create a frequncy table
 authority_freq <- as.data.frame(authority_freq)
 
 
@@ -67,46 +76,61 @@ authority_freq <- as.data.frame(authority_freq)
 #due to an appeal
 planning_permission_granted <- planning_permission[grepl("GRANT PERMISSION",planning_permission$Decision) 
                                                    & !grepl("Refuse Permission",planning_permission$AppealDecision) ,]
-str(planning_permission_granted)
-#
 
-
+#count rows in dataframe
 nrow(planning_permission_granted)
-  
+
+#convert the decision date to a character to convert it to the proper time format
 date_field <- as.character(planning_permission_granted$DecisionDate)
 
+#formatng date
 new_date <- as.Date(date_field, "%Y-%d-%m")
+#converting column in dataframe to date
 planning_permission_granted$DecisionDate <- new_date
 
+#create a year column based on decisiondate
 planning_permission_granted$year <- format(planning_permission_granted$DecisionDate, "%Y")
 
+#create a table of year values to plot
 planning_year_freq <- table(planning_permission_granted$year)
-#planning_year_freq <- as.data.frame(planning_year_freq)
 
-planning_year_freq
-
-
-
+#plot frequncy of years
 barplot(  planning_year_freq ,  
         main = "Frequency of Granted planning permission by year", 
        ylab = "Frequency", 
        xlab = "Year",
        col = "blue")
 
+#Pre 1999 has very litle data as does 2020. include data form 2000 - 2019
 planning_permission_granted <- planning_permission_granted[which(planning_permission_granted$year > 1999 
                                                           & planning_permission_granted$year < 2020) , ]
 
 
-test <- planning_permission_granted[is.na(planning_permission_granted$DecisionDate),]
+#creat a table fo datae with deciosn date between 2000 and 2019
+planning_year_freq <- table(planning_permission_granted$year)
 
-sum(!is.na(planning_permission_granted$DecisionDate))
+#plot the table fof data from 2000 - 2019
+barplot(  planning_year_freq ,  
+          main = "Frequency of Granted planning permission by year", 
+          ylab = "Frequency", 
+          xlab = "Year",
+          col = "blue")
+
+
+
+#check for na's in decision
+sum(is.na(planning_permission_granted$DecisionDate))
   
-unique(planning_permission_granted$AppealDecisionDate)
 
-planning_permission_granted[planning_permission_granted$AppealDecisionDate == "",] <- NA
+#create a cloumn based on year and month from DecionDate colukmn to match with data in POI2 CPI2 
+planning_permission_granted$month_year <- format(planning_permission_granted$DecisionDate, "%Y-%m")
+
+#Replace - with M to match the "month and year" column in POI2 and CPI2
+planning_permission_granted$month_year <- gsub("-","M",planning_permission_granted$month_year)
+planning_permission_granted$month_year
 
 
-
+cpi2$`Year and Month number`
 
 str(cpi)
 str(poi)
